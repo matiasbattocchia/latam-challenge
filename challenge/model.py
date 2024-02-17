0f564c20c39a03b88359f9bf5628838f7177dc91
Matías Battocchia <matias@riseup.net>
2024-02-17 08:20:43 -0600

    Part 1: Model (#1)
    * chose a model
    
    * model.py draft
    
    * test model passing

--- challenge/model.py
+++ challenge/model.py
@@ -2,8 +2,27 @@ import pandas as pd
 
 from typing import Tuple, Union, List
 
+from sklearn.linear_model import LogisticRegression
+
 class DelayModel:
 
+    FEATURES_COL = [
+        "OPERA_Latin American Wings", 
+        "MES_7",
+        "MES_10",
+        "OPERA_Grupo LATAM",
+        "MES_12",
+        "TIPOVUELO_I",
+        "MES_4",
+        "MES_11",
+        "OPERA_Sky Airline",
+        "OPERA_Copa Air"
+    ]
+
+    TARGET_COL = 'delay'
+
+    DELAY_THRESHOLD_MIN = 15
+    
     def __init__(
         self
     ):
@@ -13,7 +32,7 @@ class DelayModel:
         self,
         data: pd.DataFrame,
         target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
         """
         Prepare raw data for training or predict.
 
@@ -26,7 +45,25 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        return
+        # TODO: dropna
+        # TODO: missing columns
+        features = pd.concat(
+            [
+                pd.get_dummies(data['OPERA'], prefix='OPERA'),
+                pd.get_dummies(data['TIPOVUELO'], prefix='TIPOVUELO'), 
+                pd.get_dummies(data['MES'], prefix='MES')
+            ], 
+            axis = 1
+        )
+
+        if target_column:
+            target = pd.to_datetime(data['Fecha-O']) - pd.to_datetime(data['Fecha-I']) > pd.Timedelta(minutes=self.DELAY_THRESHOLD_MIN)
+            target.replace({True: 1, False: 0}, inplace=True)
+            target.name = self.TARGET_COL
+
+            return features[self.FEATURES_COL], target.to_frame()
+        
+        return features[self.FEATURES_COL]
 
     def fit(
         self,
@@ -40,7 +77,17 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
-        return
+        target = target[self.TARGET_COL]
+        
+        weights = {
+            1: sum(target == 0)/len(target),
+            0: sum(target == 1)/len(target)
+        }
+
+        model = LogisticRegression(class_weight=weights)
+        model.fit(features, target)
+
+        self._model = model
 
     def predict(
         self,
@@ -55,4 +102,5 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
-        return
+        
+        return self._model.predict(features).tolist()
