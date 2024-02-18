a88d219980f1f61a947dbef38c78b22bfe18032f
Matías Battocchia <matias@riseup.net>
2024-02-17 21:12:58 -0300

    minor stylistic improvements

--- challenge/api.py
+++ challenge/api.py
@@ -11,29 +11,29 @@ model = DelayModel()
 class Flight(BaseModel):
     # TODO: derive OPERA possible values from DelayModel
     OPERA: Literal[
-        'American Airlines',
-        'Air Canada',
-        'Air France',
-        'Aeromexico',
-        'Aerolineas Argentinas',
-        'Austral',
-        'Avianca',
-        'Alitalia',
-        'British Airways',
-        'Copa Air',
-        'Delta Air',
-        'Gol Trans',
-        'Iberia',
-        'K.L.M.',
-        'Qantas Airways',
-        'United Airlines',
-        'Grupo LATAM',
-        'Sky Airline',
-        'Latin American Wings',
-        'Plus Ultra Lineas Aereas',
-        'JetSmart SPA',
-        'Oceanair Linhas Aereas',
-        'Lacsa'
+        "American Airlines",
+        "Air Canada",
+        "Air France",
+        "Aeromexico",
+        "Aerolineas Argentinas",
+        "Austral",
+        "Avianca",
+        "Alitalia",
+        "British Airways",
+        "Copa Air",
+        "Delta Air",
+        "Gol Trans",
+        "Iberia",
+        "K.L.M.",
+        "Qantas Airways",
+        "United Airlines",
+        "Grupo LATAM",
+        "Sky Airline",
+        "Latin American Wings",
+        "Plus Ultra Lineas Aereas",
+        "JetSmart SPA",
+        "Oceanair Linhas Aereas",
+        "Lacsa"
     ]
     TIPOVUELO: Literal["N", "I"]
     MES: int = Field(ge=1, le=12)
@@ -57,7 +57,6 @@ async def get_health() -> dict:
 # we want the dicts not the models, hence Pydantic is not helping here
 @app.post("/predict", status_code=200, response_model=Prediction)
 async def post_predict(payload: Payload) -> dict:
-    print(payload)
     df = pd.DataFrame(map(dict, payload.flights))
 
     preprocessed_df = model.preprocess(df)
